f8d2d3e3e98839796c98cf02ade536fc843ec39a
Alexander Essenwanger <cibernop@mable-anex.de>
2024-08-27 16:33:43 +0200

    output the commadn before execution

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,4 +9,7 @@ OUTPUT="-html-output $PROJECT_DIR/validation.html"
 IGS=$(sed ':a;N;$!ba;s/\([a-zA-Z\-]\+:\s\+[a-z0-9\.\:\/\-]\+\n\)*dependencies:\(\(\n\s\+[a-z0-9\.]\+:\s\+[0-9a-z\.]\+\)\+\)\(\n[a-zA-Z\-]\+:\s\+[a-z0-9\.\:\/\-]\+\)*/\2/g;s/\n\s\+/\n/g;s/^\n//g;s/\([a-z0-9\.]\+\):\s\+\([0-9a-z\-\.]\+\)\n\?/ -ig \1#\2/g' $SUSHI_CONFIG)
 OWN_IG="-ig $GEN_DIR"
 
-java -jar $VAL_JAR $VERSION $IGS $OWN_IG $GEN_DIR $OUTPUT
+CMD="java -jar $VAL_JAR $VERSION $IGS $OWN_IG $GEN_DIR $OUTPUT"
+
+echo "Running $CMD"
+$CMD
